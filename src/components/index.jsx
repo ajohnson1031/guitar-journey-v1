ee910fb8bf21dc73542c601a08ffd621463c1625
ajohnson1031 <aaron.johnson1031@gmail.com>
2026-05-30 00:42:33 -0400

    add settings panel

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -8,6 +8,7 @@ export { default as GenreManager } from "./GenreManager";
 export { default as Metronome } from "./Metronome";
 export { default as RequiredChords } from "./RequiredChords";
 export { default as SessionHistory } from "./SessionHistory";
+export { default as SettingsPanel } from "./SettingsPanel";
 export { default as SongImportAssistant } from "./SongImportAssistant";
 export { default as SongSections } from "./SongSections";
 export { default as StrummingPatternBuilder } from "./StrummingPatternBuilder";
